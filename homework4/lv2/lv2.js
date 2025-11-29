6944ca41aaf0b4091b7f10f6ba9c4ce7681eb7aa
abaiqvq <abai.233@qq.com>
2025-11-29 21:47:21 +0800

    作业四（改）

--- homework4/lv2/lv2.js
+++ homework4/lv2/lv2.js
@@ -1,17 +1,21 @@
-let arr = [[1,[2,3],4],[5,[6,7],8,9]];
+let arr = [[1,[2,3],4],[5,[6,[7,11],10],8,9]];
+
 function fn(array)
 {
     let a = array.reduce((newArr,present) =>
     {
-        console.log(newArr);
-        return newArr.concat(present)
+        if(Array.isArray(present))
+        {
+            return newArr.concat(fn(present));
+        }
+        else
+        {
+            return newArr.concat(present);
+        }
     },[])
-    
-    let b = a.reduce((newArr,present) =>
-    {
-        console.log(newArr);
-        return newArr.concat(present)
-    },[])
-    return b;
+    return a;
 }
+
+
+
 console.log(fn(arr));
